618a7bf4094224f617f4e9802f6e3f673b0b8948
ANDREA <andrea.palomino.ruby@gmail.com>
2016-10-10 17:44:11 -0700

    version 2

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ var conversa=document.getElementById("conversacion");
 var input =document.getElementById("mensajes");
 var papa=document.getElementById("papa");
 var header =document.getElementById("head");
+var imagen =document.getElementById("imagen");
+var texto=document.getElementById("texto");
 window.addEventListener("load",cargarPagina);
 function cargarPagina(){
   input.addEventListener("keydown",enviar);
-  cambio();
+  var hijos =document.querySelectorAll(".w-recent-chats li .avatar");
+  
+    for (var i=0;i<hijos.length;i++){
+    	hijos[i].addEventListener("click",cambio);
+    }
 }
 function enviar(e){
 	if (e.keyCode==13){
@@ -34,17 +40,20 @@ function crear(){
     div2.innerHTML=hora+":"+min;
 }
 function cambio(){
-    var hijos =document.getElementsByClassName("avatar");
-    var contador=0;
-    for (var i=0;i<hijos.length;i++){
-    	hijos[i].addEventListener("click",function(){
-          var headerr =document.getElementById("head");
-          headerr.classList.add("none");
-          var newDiv=document.createElement("div");
-          papa.insertBefore(newDiv,papa.firstElementChild);
-          newDiv.innerHTML=hijos[i];
-        });
-   }
+          var foto =this.childNodes[1].getAttribute("src");
+          imagen.setAttribute("src",foto);
+          var nombre =this.childNodes[3].textContent;
+           texto.textContent=nombre;
+          lista.classList.add("none");
+          /*var conPadre =document.createElement("div");
+          contenedorPadre.appendChild(conPadre);
+          conPadre.classList.add("w-message-text");*/
+
+          contenedorPadre.textContent=this.childNodes[5].textContent;
+      /* contenedorPadre.classList.add("none");
+          var conPadre=document.createElement("div");
+          contenedorPadrea.appendChild(conPadre);
+          conPadre.textContent=this.childNodes[5].textContent;*/   
 }
 
 
